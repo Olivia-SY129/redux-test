c5f2c51c211f21f2cc84518ad1e3240c5022e4ef
Seung-yeon Lee <sie9412@gmail.com>
2021-05-31 23:28:41 +0900

    redux-actions

--- src/redux/modules/filter.js
+++ src/redux/modules/filter.js
@@ -1,28 +1,24 @@
-// 액션 타입 정의
-const SHOW_ALL = "redux-start/filter/SHOW_ALL";
-const SHOW_COMPLETE = "redux-start/filter/SHOW_COMPLETE";
+import { createActions, handleActions } from "redux-actions";
 
-// 액션 생성 함수
-export function showAll() {
-  return { type: SHOW_ALL };
-}
-
-export function showComplete() {
-  return { type: SHOW_COMPLETE };
-}
+export const { showAll, showComplete } = createActions(
+  "SHOW_ALL",
+  "SHOW_COMPLETE",
+  {
+    prefix: "redux-start/filter/",
+  }
+);
 
 // 초기값
 const initState = "ALL";
 
+const reducer = handleActions(
+  {
+    SHOW_ALL: (state, action) => "ALL",
+    SHOW_COMPLETE: () => "COMPLETE",
+  },
+  initState,
+  { prefix: "redux-start/filter/" }
+);
+
 // Reducer
-export default function reducer(prevState = initState, action) {
-  if (action.type === SHOW_COMPLETE) {
-    return "COMPLETE";
-  }
-
-  if (action.type === SHOW_ALL) {
-    return "ALL";
-  }
-
-  return prevState;
-}
+export default reducer;
